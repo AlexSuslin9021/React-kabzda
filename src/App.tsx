658e7827338e1f59050774b1e0f806da57d31d4b
Alexsus <alexsuslin@inbox.ru>
2022-12-25 16:46:54 +0300

    add components Raiting, Accordion, Star

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     This is App component
+        < Raiting/>
+        <Accordion/>
     </div>
   );
 }
 
+function Raiting(){
+    return(
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+
+        </div>
+    )
+}
+function Accordion(){
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+function Star(){
+    return <div>star</div>
+}
 export default App;
