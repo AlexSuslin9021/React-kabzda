ef64eb9f8c6d49672a22d520dcf09cdf81e86bb8
Alexsus <alexsuslin@inbox.ru>
2022-12-25 19:45:35 +0300

    Changed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
+import {findRenderedComponentWithType} from "react-dom/test-utils";
 
 function App() {
+    console.log('rendered App')
   return (
     <div className="App">
-     This is App component
+        <Title/>
         < Raiting/>
         <Accordion/>
+        < Raiting/>
+
     </div>
   );
 }
+function Title(){
+    return <>This is App component</>
+}
 
 function Raiting(){
+    console.log('rendered Raiting')
     return(
         <div>
             <Star/>
@@ -24,19 +32,30 @@ function Raiting(){
     )
 }
 function Accordion(){
+    console.log('rendered Accordion')
     return(
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
 
+function AccordionTitle(){
+    console.log('rendered AccordionTitle')
+    return<h3>Menu</h3>
+}
+function AccordionBody(){
+    console.log('rendered AccordionBody')
+   return( <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>)
+}
+
 function Star(){
+    console.log('rendered Star')
     return <div>star</div>
 }
 export default App;
